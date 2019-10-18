298dab86d453da9bcb78a255d700d9b32e1e677b
Alexander Kostyurenko <alekkostyur@gmail.com>
2019-10-19 00:01:50 +0300

    Initial commit

---
+++ src/test/java/org/turingsquad/springsandbox/SpringSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.turingsquad.springsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
