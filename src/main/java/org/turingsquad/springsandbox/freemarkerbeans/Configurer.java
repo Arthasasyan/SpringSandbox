35ac3fb6a01cc915906fd7c6d3c1fc5999d3339a
Alexander Kostyurenko <alekkostyur@gmail.com>
2019-11-05 14:37:32 +0300

    Add FreeMarker config

---
+++ src/main/java/org/turingsquad/springsandbox/freemarkerbeans/Configurer.java
@@ -0,0 +1,23 @@
+package org.turingsquad.springsandbox.freemarkerbeans;
+
+import freemarker.template.TemplateException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactory;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import java.io.IOException;
+
+@Configuration
+public class Configurer {
+    @Bean
+    public FreeMarkerConfigurer freemarkerConfig() throws IOException, TemplateException {
+        FreeMarkerConfigurationFactory factory = new FreeMarkerConfigurationFactory();
+        factory.setTemplateLoaderPaths("classpath:templates", "src/main/resource/templates");
+        factory.setDefaultEncoding("UTF-8");
+        FreeMarkerConfigurer result = new FreeMarkerConfigurer();
+        result.setConfiguration(factory.createConfiguration());
+        return result;
+    }
+
+}
---
+++ src/main/java/org/turingsquad/springsandbox/freemarkerbeans/View.java
@@ -0,0 +1,20 @@
+package org.turingsquad.springsandbox.freemarkerbeans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+@Configuration
+public class View {
+
+    @Bean
+    public ViewResolver freemarkerViewResolver() {
+        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+        resolver.setCache(true);
+        resolver.setPrefix("");
+        resolver.setSuffix(".ftl");
+        resolver.setContentType("text/html; charset=UTF-8");
+        return resolver;
+    }
+}
